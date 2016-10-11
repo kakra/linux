43f3aefe665ee23de9118840049fe070b0a10658
Eric Wheeler <git@linux.ewheeler.net>
2016-10-11 12:04:52 -0700

    bcache: introduce bcache sysfs entries for ioprio-based bypass/writeback hints
    Add sysfs entries to support to hint for bypass/writeback by the ioprio
    assigned to the bio.  If the bio is unassigned, use current's io-context
    ioprio for cache writeback or bypass (configured per-process with
    `ionice`).
    
    Having idle IOs bypass the cache can increase performance elsewhere
    since you probably don't care about their performance.  In addition,
    this prevents idle IOs from promoting into (polluting) your cache and
    evicting blocks that are more important elsewhere.
    
    If you really nead the performance at the expense of SSD wearout,
    then configure ioprio_writeback and set your `ionice` appropriately.
    
    For example:
    	echo 2,7 > /sys/block/bcache0/bcache/ioprio_bypass
    	echo 2,0 > /sys/block/bcache0/bcache/ioprio_writeback
    
    See the documentation commit for details.
    
    Signed-off-by: Eric Wheeler <bcache@linux.ewheeler.net>
    Acked-by: Kent Overstreet <kent.overstreet@gmail.com>
    Tested-by: Kai Krakow <kai@kaishome.de>
    Cc: nix@esperi.org.uk
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- drivers/md/bcache/request.c
+++ drivers/md/bcache/request.c
@@ -367,6 +367,8 @@ static bool check_should_bypass(struct cached_dev *dc, struct bio *bio)
 	unsigned int sectors, congested;
 	struct task_struct *task = current;
 	struct io *i;
+	struct io_context *ioc;
+	unsigned short ioprio;
 
 	if (test_bit(BCACHE_DEV_DETACHING, &dc->disk.flags) ||
 	    c->gc_stats.in_use > CUTOFF_CACHE_ADD ||
@@ -392,6 +394,28 @@ static bool check_should_bypass(struct cached_dev *dc, struct bio *bio)
 		if (!(bio->bi_opf & (REQ_META|REQ_PRIO)) &&
 		    (dc->cache_readahead_policy != BCH_CACHE_READA_ALL))
 			goto skip;
+	}
+
+	/* If the ioprio already exists on the bio, use that.  We assume that
+	 * the upper layer properly assigned the calling process's ioprio to
+	 * the bio being passed to bcache. Otherwise, use current's ioc. */
+	ioprio = bio_prio(bio);
+	if (!ioprio_valid(ioprio)) {
+		ioc = get_task_io_context(current, GFP_NOIO, NUMA_NO_NODE);
+		if (ioc) {
+			if (ioprio_valid(ioc->ioprio))
+				ioprio = ioc->ioprio;
+			put_io_context(ioc);
+			ioc = NULL;
+		}
+	}
+
+	/* If process ioprio is lower-or-equal to dc->ioprio_bypass, then
+	 * hint for bypass. Note that a lower-priority IO class+value
+	 * has a greater numeric value. */
+	if (ioprio_valid(ioprio) && ioprio_valid(dc->ioprio_writeback)
+		&& ioprio >= dc->ioprio_bypass) {
+		goto skip;
 	}
 
 	if (bio->bi_iter.bi_sector & (c->cache->sb.block_size - 1) ||
