04c811b198b0578b9a002cd19504de92b63e00d9
Kai Krakow <kai@kaishome.de>
2021-01-01 05:22:38 +0100

    xpadneo: Merge upstream changes
    Signed-off-by: Kai Krakow <kai@kaishome.de>

---
+++ drivers/hid/xpadneo/consumer.c
@@ -0,0 +1,32 @@
+/*
+ * xpadneo consumer control driver
+ *
+ * Copyright (c) 2021 Kai Krakow <kai@kaishome.de>
+ */
+
+#include "xpadneo.h"
+
+extern int xpadneo_init_consumer(struct xpadneo_devdata *xdata)
+{
+	struct hid_device *hdev = xdata->hdev;
+	int ret, synth = 0;
+
+	if (!xdata->consumer) {
+		synth = 1;
+		ret = xpadneo_init_synthetic(xdata, "Consumer Control", &xdata->consumer);
+		if (ret || !xdata->consumer)
+			return ret;
+	}
+
+	if (synth) {
+		ret = input_register_device(xdata->consumer);
+		if (ret) {
+			hid_err(hdev, "failed to register consumer control\n");
+			return ret;
+		}
+
+		hid_info(hdev, "consumer control added\n");
+	}
+
+	return 0;
+}
